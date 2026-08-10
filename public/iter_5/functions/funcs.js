0099dcbbcd099c9f0b654b238887e23f3b4b44b8
not-that-python <avni.k.balan@gmail.com>
2026-08-10 18:34:21 +0100

    created initWeightBiasLayer and initWeightBiasNetwork, both untested

--- public/iter_5/functions/funcs.js
+++ public/iter_5/functions/funcs.js
@@ -25,3 +25,31 @@ export function createNetwork(dimensions) {
 
     return network
 }
+
+export function initWeightBiasLayer(numNodes, prevNumNodes, inputLayer) {
+    if (inputLayer) {throw new Error("Input layer does not have a weight matrix or a bias")}
+    // input validation for input layers just in case
+
+    let weightMatrix = []
+    for (let i=0; i<numNodes; i++) {weightMatrix[i] = []} // first rows (height)
+    for (let j=0; j<numNodes; j++) {weightMatrix[i][j] = (Math.random()*2)-1} // then values (width)
+    // we'll see if there are referencing errors later or not, I don't think there will be
+
+    let bias = 0
+    return weightMatrix, bias
+}
+
+export function initWeightBiasNetwork(network) {
+    let newNetwork = structuredClone(network)
+
+    for (let i=1; i<newNetwork.length; i++) {
+        let layer = newNetwork[i]
+        let prevLayer = newNetwork[i-1]
+
+        let weightMatrix, bias = initWeightBiasLayer(layer.nodes.length, prevLayer.nodes.length, layer.inputLayer)
+        layer.weightMatrix = weightMatrix
+        layer.bias = bias
+    }
+
+    return newNetwork
+}
