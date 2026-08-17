8da02e4942906a5e8aee7ad982487bb4ebdd595a
not-that-python <avni.k.balan@gmail.com>
2026-08-17 16:50:18 +0100

    tested and confirmed that drawing to the canvas works

--- public/iter_5/script.js
+++ public/iter_5/script.js
@@ -1,2 +1,9 @@
 import * as func from './functions/funcs.js'
 import * as tf from '@tensorflow/tfjs'
+
+// get relevant elements from the DOM
+let ctx = canvas.getContext('2d')
+
+// testing drawing to the canvas
+// ctx.fillStyle = 'red'
+// ctx.fillRect(0, 0, 120, 120)
